7a21bfc51d04ebf7a0817685247d4cbb89e2d458
ligj100 <liguojun518@163.com>
2015-08-26 17:10:32 +0800

    添加插件机制

---
+++ disk.core/DiskVersion.cs
@@ -0,0 +1,17 @@
+﻿
+namespace disk.Core
+{
+    public static class DiskVersion
+    {
+        /// <summary>
+        /// Gets or sets the store version
+        /// </summary>
+        public static string CurrentVersion 
+        {
+            get
+            {
+                return "1.00";
+            }
+        }
+    }
+}
