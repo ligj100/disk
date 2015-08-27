bb378668df1745efe43984dcc478f8d0eae7da34
ligj100 <ligj411612>
2015-08-27 20:39:38 +0800

    添加静态文件

--- disk.web/App_Start/BundleConfig.cs
+++ disk.web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace disk.web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery*.js",
+                        "~/Scripts/hideshow.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,8 +25,8 @@ namespace disk.web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/site.css"));
         }
     }
 }
